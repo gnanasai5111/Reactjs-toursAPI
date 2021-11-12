55b171821dd95cbb0441dc6ac5f67e44f8e5a892
gnanasai <gnanasai5111@gmail.com>
2021-11-12 14:09:14 +0530

    Updates

---
+++ precache-manifest.75ddab596f4808807d9e455b5116ee6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ab37d1707e4ce52c26e1417fc42842",
+    "url": "/Reactjs-toursAPI/index.html"
+  },
+  {
+    "revision": "b3bbeec38b2a2ef877b2",
+    "url": "/Reactjs-toursAPI/static/css/main.c3e53c0d.chunk.css"
+  },
+  {
+    "revision": "d1ccc1e6d556dbe2d9b7",
+    "url": "/Reactjs-toursAPI/static/js/2.d8a71053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Reactjs-toursAPI/static/js/2.d8a71053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3bbeec38b2a2ef877b2",
+    "url": "/Reactjs-toursAPI/static/js/main.b6d5fd7e.chunk.js"
+  },
+  {
+    "revision": "7b3b4e58115d963dfb6c",
+    "url": "/Reactjs-toursAPI/static/js/runtime-main.c08e9d31.js"
+  }
+]);
